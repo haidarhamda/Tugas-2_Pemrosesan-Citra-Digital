5e53a531455945c854ede7a463d3d5d5c6cf7ae2
haidarhamda <haidarhamda23@gmail.com>
2024-10-24 22:25:04 +0700

    Update convolution.m

--- convolution.m
+++ convolution.m
@@ -4,26 +4,28 @@ function [h]=convolution(f,g)
     h=zeros(n,m,c);
     padx=floor(x/2);
     pady=floor(y/2);
-    paddedf=padarray(f,[padx,pady],'replicate');
+    paddedf=padarray(f,[padx,pady]);
 
     for channel=1:c
         imgChannel = paddedf(:,:,channel);
-        imshow(imgChannel)
-        for i=2:n-1
-            for j=2:m-1
-                tmp=imgChannel(i:i+x-1,j:j+y-1);
-                h(i,j)=sum(sum(tmp.*g));
+        % imshow(imgChannel)
+        tmpChannel=zeros(n,m);
+        disp(channel);
+        for i=2:n
+            for j=2:m
+                tmp=double(imgChannel(i:i+x-1,j:j+y-1));
+                tmpChannel(i,j)=sum(sum(tmp.*double(g)));
             end
         end
+        h(:,:,c)=tmpChannel;
     end
     
 end
 
-% img = imread("img\camera.bmp");
-% g=[-1 -1 -1; -1 8 -1; -1 -1 -1];
-% h=uint8(convolution(double(img),double(g)));
-% 
-% 
-% imshow(h)
-% hm=uint8(conv2(double(img), double(g)));
+img = imread("img\shore.jpg");
+g=[-1 -1 -1; -1 8 -1; -1 -1 -1];
+h=(convolution(im2double(img),g));
+
+imshow(h)
+% hm=uint8(conv2(im2double(img), double(g)));
 % figure,imshow(hm)
