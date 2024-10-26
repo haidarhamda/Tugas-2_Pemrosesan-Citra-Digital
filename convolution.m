ce2497b6fc4a23e6a7451e5fd323e505d70e4a2f
haidarhamda <haidarhamda23@gmail.com>
2024-10-26 09:05:48 +0700

    Update convolution.m

--- convolution.m
+++ convolution.m
@@ -1,21 +1,36 @@
 function [h]=convolution(f,g)
     [n, m, c] = size(f);
     [x,y]=size(g);
-    h=double(zeros(n,m,c));
+    
     padx=floor(x/2);
     pady=floor(y/2);
+    h=double(zeros(n,m,c));
     paddedf=padarray(f,[padx,pady]);
-
+    % disp(size(paddedf));
     for channel=1:c
         imgChannel = paddedf(:,:,channel);
         tmpChannel=double(zeros(n,m));
-        for i=1+padx:n-padx
-            for j=1+pady:m-pady
-                tmp=double(imgChannel(i:i+x-padx,j:j+y-pady));
-                tmpChannel(i+padx,j+pady)=sum(sum(tmp.*double(g)));
+        tmpChannel=padarray(tmpChannel,[padx,pady]);
+        % disp(size(tmp));
+        for i=1+padx:n+padx
+            for j=1+pady:m+pady
+                tmp=double(imgChannel(i-padx:i+padx,j-pady:j+pady));
+                
+                % tmpChannel(i+padx,j+pady)=sum(sum(tmp.*double(g)));
+                % disp(size(tmp));
+                tmpChannel(i,j)=sum(sum(tmp.*double(g)));
+                
             end
         end
-        h(:,:,channel)=tmpChannel;
+        % disp(h(:,:,channel));
+        % disp(tmpChannel(4,:));
+        % disp(size(tmpChannel))
+        % disp(n+padx)
+
+        h(:,:,channel)=tmpChannel(padx+1:n+padx,pady+1:m+pady);
+        % disp(h(:,:,channel));
+        % disp(h(4,:,channel));
+        % disp(size(h(:,:,channel)));
     end
 end
 
