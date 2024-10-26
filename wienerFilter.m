0004c906ba0987653d8a445f4cce24b30860a095
Ammar Rasyad Chaeroel <ammarasyad@gmail.com>
2024-10-26 21:12:44 +0700

    Add wiener filter to app

--- wienerFilter.m
+++ wienerFilter.m
@@ -1,8 +1,5 @@
-function [blurred, result]=wienerFilter(filename, nsr)
-    img = imread(filename);
-    img = im2double(img);
-
-    [blurred, kernel] = motionBlur(img, 20, 45);
+function [blurred, result]=wienerFilter(img, length, theta, nsr)
+    [blurred, kernel] = motionBlur(img, length, theta, nsr);
 
     [x, y, z] = size(img);
     result = zeros(size(img));
@@ -10,10 +7,10 @@ function [blurred, result]=wienerFilter(filename, nsr)
     for i = 1:z
         blurred_channel = blurred(:, :, i);
         blurred_fft = fft2(blurred_channel);
-        kernel_padded = padarray(kernel, [x - 20, y - 20], 'post');
+        kernel_padded = padarray(kernel, [x - length, y - length], 'post');
         kernel_fft = fft2(kernel_padded);
 
-        H = conj(kernel_fft) ./ (abs(kernel_fft).^2 + nsr);
+        H = conj(kernel_fft) ./ (abs(kernel_fft).^2 + nsr / 10);
 
         result_channel_fft = blurred_fft .* H;
         result_channel = ifft2(result_channel_fft);
@@ -21,7 +18,7 @@ function [blurred, result]=wienerFilter(filename, nsr)
     end
 end
 
-function [result, kernel]=motionBlur(img, length, theta)
+function [result, kernel]=motionBlur(img, length, theta, nsr)
     kernel = zeros(length, length);
     center = length / 2;
     for i = 1:length
@@ -29,6 +26,7 @@ function [result, kernel]=motionBlur(img, length, theta)
         y = round(center + (i - center) * sind(theta));
         kernel(y, x) = 1;
     end
+    kernel = kernel + nsr * randn(size(kernel));
     kernel = kernel / sum(kernel(:));
 
     [x, y, z] = size(img);
